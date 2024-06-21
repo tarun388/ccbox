3653b802bfddadaaec18e662b4279abcf5b60004
Tarun Verma <36099367+tarun388@users.noreply.github.com>
2024-06-21 16:11:04 +0530

    init

---
+++ src/main/java/com/verma/tarun/ccbox/CcboxApplication.java
@@ -0,0 +1,13 @@
+package com.verma.tarun.ccbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verma/tarun/ccbox/CcboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verma.tarun.ccbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
